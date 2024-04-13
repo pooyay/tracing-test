fbc02cbb10d1e82f74444e32421cbc514f63fa3e
Pooya Yousefi <pooya.yousefi@snapp.cab>
2024-04-13 10:46:07 +0330

    consumerjob is in consumer.go

---
+++ consumer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+)
+
+func consumerJob(ctx context.Context) {
+	_, span := tracer.Start(ctx, "consumer")
+	defer span.End()
+
+	nc := Nc.nc
+
+	js, err := initJetStream(nc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stream, err := createStream(ctx, js)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c, err := createDurableConsumer(ctx, stream)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Consume messages continuously
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			// Get the message from the consumer
+			msg, err := c.Next()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			msg.Ack()
+			fmt.Println("Received a JetStream message: ", string(msg.Data()))
+		}
+	}
+}
